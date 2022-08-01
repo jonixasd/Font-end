119bbf85b0252ec18fb6c0dc84a2dccbdf727a86
jonixasd <k94923k@gmail.com>
2022-08-01 23:30:17 +0800

    myfirstpost

---
+++ index/index.js
@@ -0,0 +1,25 @@
+console.log("hello world");
+console.log(20);
+console.log("hello world");
+console.log(20 / 10);
+console.log(20 * 10);
+console.log(100 - 50);
+console.log(100 + 50);
+console.log("place" + "black");
+console.log("sdf" + " " + "asd");
+console.log("asd" + "," + "ikdfg");
+// length:字串裡的字數
+console.log("sdlfjh;asd".length);
+// toUpperCase:換成大寫
+console.log("jonix".toUpperCase());
+console.log("hello".toUpperCase());
+// startsWith:檢查是否大寫
+console.log("hey".startsWith());
+// trim:去除空格
+console.log("   white".trim());
+// math.random:生成隨機數
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 43.8));
+console.log(Math.ceil(43.8));
+// isInteger:檢查是否整數
+console.log(Number.isInteger(2017));
