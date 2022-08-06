6567bf6cf2b1a831f936342bb8f7c19276d2bb58
jonixasd <k94923k@gmail.com>
2022-08-06 17:18:34 +0800

    javascript

--- index/index.js
+++ index/index.js
@@ -1,19 +1,77 @@
 console.log("String");
+
 console.log(200);
 console.log(200 + 10);
 console.log(200 - 10);
 console.log(200 * 10);
 console.log(200 / 10);
 console.log(11 % 3);
+
 console.log("hello" + "world");
 console.log("siren" + " " + "matire");
-// .length 字數
+// .length
 console.log("lkjasgfdkojadsgfoi;j".length);
-// .toUpperCase 大寫
+// .toUpperCase
 console.log("sirenmatire".toUpperCase());
-// .trim 去除頭尾空格
+// .trim
 console.log(" siren mati re ".trim());
+
 console.log(Math.floor(Math.random() * 100));
 console.log(Math.floor(Math.random() * 50));
 console.log(Math.random() * 100);
+
 console.log(Number.isInteger(2017));
+
+var favoriteFood = "pizza";
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+var siren = "warning";
+console.log(siren);
+
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
+
+const entree = "Enchiladas";
+console.log(entree);
+
+//! ERROR
+/* const Error example = "Error example";
+console.log(Error example);
+Error example = "Error exp";
+*/
+
+let levelUp = 10;
+levelUp += 5;
+let powerLevel = 9001;
+powerLevel -= 100;
+let multiplyMe = 32;
+multiplyMe *= 11;
+let quarterMe = 1152;
+quarterMe /= 4;
+console.log(levelUp);
+console.log(powerLevel);
+console.log(multiplyMe);
+console.log(quarterMe);
+
+let gainedDollar = 3;
+// +-1
+gainedDollar++;
+let lostDollar = 50;
+lostDollar--;
+console.log(gainedDollar);
+console.log(lostDollar);
+
+let favoriteAnimal = "dog";
+console.log("My fav animal" + " " + favoriteAnimal);
+console.log("My favorite animal:" + favoriteAnimal);
+
+var myName = "Jonix";
+var myCity = "Tainan";
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+
+var iAM16y = "sixteen years old";
+var thethingLIKEtodo = "skateboarding";
+console.log(`I am ${iAM16y} now. I like ${thethingLIKEtodo}.`);
